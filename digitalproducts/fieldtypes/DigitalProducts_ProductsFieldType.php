0d14e6eeb1c8fe160789851b53fab5d2776c99ad
Andris Ševčenko <andris@pixelandtonic.com>
2016-04-26 11:30:33 +0300

    Add fieldtype.

---
+++ digitalproducts/fieldtypes/DigitalProducts_ProductsFieldType.php
@@ -0,0 +1,44 @@
+<?php
+namespace Craft;
+
+/**
+ * Class DigitalProducts_ProductsFieldType
+ *
+ * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @copyright Copyright (c) 2016, Pixel & Tonic, Inc.
+ */
+class DigitalProducts_ProductsFieldType extends BaseElementFieldType
+{
+    // Properties
+    // =========================================================================
+
+    /**
+     * The element type this field deals with.
+     *
+     * @var string $elementType
+     */
+    protected $elementType = 'DigitalProducts_Product';
+
+    /**
+     * @inheritDoc IComponentType::getName()
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return Craft::t('Digital Products');
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc BaseElementFieldType::getAddButtonLabel()
+     *
+     * @return string
+     */
+    protected function getAddButtonLabel()
+    {
+        return Craft::t('Add a product');
+    }
+}
