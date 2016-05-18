9b03d87acc3215c2082b97550b3ad5b34cc19b80
Andris Ševčenko <andris@pixelandtonic.com>
2016-05-18 16:32:52 +0300

    Remove unused property.

--- digitalproducts/DigitalProductsPlugin.php
+++ digitalproducts/DigitalProductsPlugin.php
@@ -11,7 +11,6 @@ namespace Craft;
  */
 class DigitalProductsPlugin extends BasePlugin
 {
-    public $handle = 'digitalProducts';
 
     // Public Methods
     // =========================================================================
