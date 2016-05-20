785d8029407cf62c28aab3baa0a23d0ac9f22606
Andris Ševčenko <andris@pixelandtonic.com>
2016-05-20 10:03:04 +0300

    Wording.

--- digitalproducts/config.php
+++ digitalproducts/config.php
@@ -3,7 +3,7 @@ return [
     // If a user should be assigned licenses on registration belonging to his email address.
     'autoAssignLicensesOnUserRegistration' => true,
 
-    // Should a user be assigned a license if a purchase is being made by a non-logged in user with the user's e-mail address.
+    // Should a user be assigned the license if a purchase is being made by a non-logged in user with the user's e-mail address.
     'autoAssignUserOnPurchase' => true,
 
     // The alphabet to use for license keys
